afd6e2ad7e5e5f25e6f646f9ecb65fc40c15f453
wuqinqiang <1185079673@qq.com>
2022-04-20 10:47:33 +0800

    feat:random number

---
+++ tools/random/random.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"math/rand"
+	"reflect"
+	"time"
+	"unsafe"
+)
+
+// GetRandomNumber  get numbers for random
+func GetRandomNumber(l int) string {
+	str := "0123456789"
+	bytes := []byte(str)
+	var result []byte
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return BytesToString(result)
+}
+
+// BytesToString 0 copy from []byte to string
+func BytesToString(b []byte) (s string) {
+	_bptr := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	_sptr := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	_sptr.Data = _bptr.Data
+	_sptr.Len = _bptr.Len
+	return s
+}
